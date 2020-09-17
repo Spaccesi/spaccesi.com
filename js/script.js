40866aa6b153e26e0662df8356f198b3ffc6b0b8
Spaccesi <agustinspaccesi@gmail.com>
2020-09-17 18:48:34 +0200

    New project page

--- js/script.js
+++ js/script.js
@@ -46,9 +46,56 @@ function openImg(imgs) {
   expandImg.parentElement.style.display = "block";
  }
 
+
+//Toats
 $(document).ready(function(){
     $(".show-toast").click(function(){
         $("#myToast").toast({ autohide: false });
         $("#myToast").toast('show');
     }); 
 });
+
+
+
+//Portfolio sort
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("column");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+function filterSelection1(hash) {
+  var x, i;
+  x = document.getElementsByClassName("btn");
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], " active1");
+    if (x[i].className.indexOf(hash) > -1) w3AddClass(x[i], " active1");
+  }
+}
+
+
